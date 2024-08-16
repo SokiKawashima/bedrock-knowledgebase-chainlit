ca6ec9c28d3da00fe3c8e91fc0dfa788f08455d6
SokiKawashima <120550971+SokiKawashima@users.noreply.github.com>
2024-08-16 23:12:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+## Get Started
+## デプロイ
+
+### 前提条件
+デプロイには以下のソフトウェアがインストールされている必要があります．  
+以下のソフトウェアがインストールされている必要があります．
+* Python 3.12
+* pipenv
+* Docker
+* AWS Copilot
+
+### 概要
+1. ECRにDockerイメージをpush
+2. ECSでタスクとサービスを定義
+3. ECSでタスクをrun
+これをAWS Copilotで簡易的にIaC化している感じ
+
+### 具体的な順序
+1. アプリケーションの初期化
+```sh
+$ copilot init app
+Application name: 【好きなアプリ名を入力】
+Workload type: 【Load Balanced Web Serviceを選択】
+Service name: 【好きなサービス名を入力】
+Dockerfile: 【./Dockerfileを選択】
+Environment: 【devとかお好きに】
+```
+2. 提供されたURLにアクセス
+3. 再デプロイ
+```sh
+$ copilot deploy
+$ copilot deploy --env ssl-dev #test-srmt.comにデプロイしたいときはこっち！
+$ copilot svc status #デプロイの進行状況の確認
+$ copilot svc log #新しくデプロイされたサービスのログの確認
+```
+
+## ローカルで開発
+1. Dockerfile作成
+```sh
+$ touch Dockerfile
+```
+Dockerfile.exampleを参考にAWSCredentials（AWSの認証情報）は自分の情報を入力
+2. Dockerイメージをbuild
+```sh
+$ docker build -t axt2024ragchat:latest . 
+```
+3. Dockerアプリをrun
+```sh
+$ docker run -p 8080:8080 axt2024ragchat:latest
+```
+
+## ユーザ名とパスワード
+実はまだハードコーディングしてる（いつかOAuth実装する）  
+
+## リソースのクリーンアップ
+```sh
+copilot app delete
+```
---
+++ app.py
@@ -0,0 +1,86 @@
+import json
+from typing import Optional
+
+import boto3
+import chainlit as cl
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    if (username, password) == ("example@demo.com", "demo_password"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("history", "")
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    try:
+        kb = boto3.client("bedrock-agent-runtime", region_name="us-east-1")
+
+        chat_history = cl.user_session.get("history")
+        prompt = f"""# ユーザの入力
+{message.content}
+
+# ここまでの会話履歴
+{chat_history}
+"""
+        params = {
+            "input": {"text": prompt},
+            "retrieveAndGenerateConfiguration": {
+                "type": "KNOWLEDGE_BASE",
+                "knowledgeBaseConfiguration": {
+                    "knowledgeBaseId": "JI0ENDTKOI",
+                    "modelArn": "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+                    "retrievalConfiguration": {
+                        "vectorSearchConfiguration": {"numberOfResults": 20},
+                    },
+                },
+            },
+        }
+
+        response = kb.retrieve_and_generate(**params)
+
+        citations = response["citations"]
+        answers = []
+        text_elements = []
+
+        source_index = 1
+        for citation_idx, citation in enumerate(citations):
+            answer = citation["generatedResponsePart"]["textResponsePart"]["text"]
+            for ref_idx, reference in enumerate(citation["retrievedReferences"]):
+                content_text = reference["content"]["text"]
+                url = reference["location"]["s3Location"]["uri"]
+                source_name = f"[{source_index}]"
+                source_index += 1
+                text_elements.append(
+                    cl.Text(
+                        content=f"Content: {content_text}\n\nSource URL: {url}",
+                        name=source_name,
+                        display="side",
+                    )
+                )
+                answer += f"\n{source_name}"
+            answers.append(answer)
+
+        answers = "\n".join(answers)
+        await cl.Message(content=answers, elements=text_elements).send()
+        chat_history += (
+            f"\nHuman: {message.content} \nBot: {response['output']['text']}"
+        )
+    except Exception as e:
+        await cl.Message(
+            content=f"Error: {e}",
+        ).send()
+    finally:
+        cl.user_session.set("history", chat_history)
